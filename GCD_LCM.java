5f8edf60c777df74bb209bf9cfe7c093b07744a7
Naresh Kumar Reddy Tatigutla <nareshkumarreddytatigutla@gmail.com>
2025-12-14 15:48:01 +0530

    Create GCD_LCM.java

---
+++ GCD_LCM.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class GCD_LCM {
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int gcd = gcd(a, b);
+        int lcm = (a * b) / gcd;
+        System.out.println("GCD = " + gcd);
+        System.out.println("LCM = " + lcm);
+    }
+}
